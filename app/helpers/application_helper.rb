581df0da6598e390306c026be2514f9288cdb927
Anton Kravchenko <anton.kravcenko@gmail.com>
2009-04-06 15:59:43 +0400

    Категории - древовидная структура

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def render_category_with_all_childs(category)
+    res = "<li>"
+    res += h category.name
+    if category.children.size > 0
+      res += "<ul>"
+      category.children.each do |child|
+        res += render_category_with_all_childs(child)
+      end
+      res += "</ul>"
+    end
+    res += "</li>"
+    res
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,11 @@ class Category < ActiveRecord::Base
   acts_as_category
   
   has_many :companies
+
+  def self.all_for_select(except=nil)
+    conditions = except.nil? ? [] : ['id <> ?', except]
+    categories = self.find(:all, :conditions => conditions).collect { |cat| ["#{'-'*cat.ancestors_count} #{cat.name}", cat.id] }
+    categories << ["-", nil]
+  end
+  
 end
