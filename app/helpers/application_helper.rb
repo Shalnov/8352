9487160d45f0ba7ed3ada0c7e177212a5f8cef7e
Anton Kravchenko <anton.kravcenko@gmail.com>
2009-04-10 01:30:20 +0400

    Категории - администрирование + тесты

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@ module ApplicationHelper
   def render_category_with_all_childs(category, admin=false, description=false)
     res = "<li>"
     res += h category.name
-    res += " " + link_to('Изменить', edit_category_path(category)) if admin
-    res += " | " + link_to('Удалить', category, :confirm => 'Удалить категорию?', :method => :delete) if admin
+    res += " " + link_to('Изменить', edit_admin_category_path(category)) if admin
+    res += " | " + link_to('Удалить', admin_category_path(category), :confirm => 'Удалить категорию?', :method => :delete) if admin
     res += "<br />" + h(category.description) if description
     if category.children.size > 0
       res += "<ul>"
--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -7,4 +7,36 @@ class CategoryTest < Test::Unit::TestCase
   
   should_validate_presence_of :name
   should_validate_presence_of :description
+
+  context "A Category class" do
+    setup do
+      Category.destroy_all
+      @category_1     = Factory.create(:category)
+      @category_2     = Factory.create(:category)
+      @category_3     = Factory.create(:category)
+      @category_2_1   = Factory.create(:category, :parent_id => @category_2.id, :name => 'category_2_1')
+      @category_2_2   = Factory.create(:category, :parent_id => @category_2.id, :name => 'category_2_2')
+      @category_2_1_1 = Factory.create(:category, :parent_id => @category_2_1.id, :name => 'category_2_1_1')
+    end
+
+    should "return sorted categories" do
+      assert_equal 6, Category.find(:all).size
+      assert_equal @category_2.id, @category_2_2.parent.id
+      sorted_categories = Category.sorted
+      [@category_1, @category_2, @category_2_1, @category_2_1_1, @category_2_2, @category_3].each_with_index do |cat, i|
+        assert_equal cat, sorted_categories[i]
+      end
+    end
+
+    should "return categories for select except defined" do
+      selected = Category.all_for_select(@category_2_1)
+      [nil, @category_1, @category_2, @category_2_1_1, @category_2_2, @category_3].each_with_index do |cat, i|
+        if cat
+          assert_equal cat.id, selected[i].last
+        else
+          assert_equal nil, selected[i].last
+        end
+      end
+    end
+  end
 end
