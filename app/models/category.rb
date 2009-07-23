78a84a628ee602f9ddb336ac471d3799ec8f1993
Danil Pismenny <danil@orionet.ru>
2009-07-23 21:55:19 +0400

    deploy commit

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 class Category < ActiveRecord::Base
   
-#  extend ActiveSupport::Memoizable 
+  extend ActiveSupport::Memoizable 
   
   # TODO Попытаться отключить обновление разных параметров при каждом добавлении категории
   
@@ -22,14 +22,14 @@ class Category < ActiveRecord::Base
     self.hidden=nil# unless self.hidden
   end
   
-#  memoize :children
+  memoize :children
 
-#  class << self 
-#    extend ActiveSupport::Memoizable 
+  class << self 
+    extend ActiveSupport::Memoizable 
     
-#   memoize :roots
+   memoize :roots
 
-#  end 
+  end 
 #   define_index do
 #     indexes :name
 #     indexes description
