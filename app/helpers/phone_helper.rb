f1d3f039dcfc58494e292e1235fbbd51e6fedaad
Danil Pismenny <danil@orionet.ru>
2009-08-17 13:27:06 +0400

    удалил лишнее с phone_helper

--- app/helpers/phone_helper.rb
+++ app/helpers/phone_helper.rb
@@ -10,39 +10,6 @@ module PhoneHelper
     else
       "7 (#{str[1..4]}) #{str[5..6]}-#{str[7..8]}-#{str[9..10]}"
     end
-  end
-
-    
-  
-  # # Достает из строки номер, и его обозначение
-  
-  # def parse_phone(str,prefix=nil)
-  #   return if str.blank?
-    
-  #   str=str.to_s.strip
-    
-  #   h={
-  #     :number=>nil,
-  #     :is_fax=>false,
-  #     :department=>nil
-  #   }
-  #   number = str.gsub(/[^0-9]+$/,'').gsub(/^[^0-9]+/,'')
-
-  #   h[:number] = normalize_phone(number,prefix) || return
-    
-  #   # TODO сделать обработку ошибоку raise("ERROR: Can't recognize phone number from string '#{str}'")
-    
-  #   department = str.gsub(/[0-9\-\ \(\)+]+$/,'').gsub(/^[0-9\-\ \(\)+]+/,'');
-
-  #   if department=~/факс/
-  #     h[:is_fax]=true
-  #   else
-  #     h[:department]=department
-  #   end
-  
-  #   h
-  # end
-  
-
+  end    
 
 end
