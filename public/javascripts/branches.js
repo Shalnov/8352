16b96de15edec028a4e8016f03fff324d0ff9dad
unknown <@.(none)>
2009-09-02 10:42:00 +0400

    Prefinal branches release

--- public/javascripts/branches.js
+++ public/javascripts/branches.js
@@ -1,7 +1,29 @@
 function branches() {
-  $('#branches tr').draggable('destroy');
-  $('#branches tr').droppable('destroy');
-  $('#branches tr ._branch').droppable('destroy');
+/*  $('#branches tr ._branch').droppable('destroy');
+  $('#branches tr ._branch').draggable('destroy');
+  $('#branches tr ._group').draggable('destroy');  
+  $('#groups tr ._group').draggable('destroy');    */
+
+  $('#branches tr._branch').droppable({
+    accept: 'tr._branch, tr._group',
+    hoverClass: 'hover',
+    drop: function(event, ui) {
+      $.ajax({
+        type: 'post',
+        dataType: 'script',
+        data: {
+          clone: $(ui.helper).hasClass('gclone'),
+          source: $(ui.draggable).attr('id'),
+          target: $(this).attr('id'),
+          authenticity_token: window._token
+        },
+        url: '/admin/branches/move',
+        error: function() {
+          alert('error')
+        }
+      });
+    }
+  });
 
   $('#branches tr._branch').draggable({
     revert: 'invalid',
@@ -10,41 +32,16 @@ function branches() {
       return "<div class='helper'>"+$(this).attr('rel')+"</div>"
     }
   });
-
-  $('#branches tr._branch').droppable({
-    accept: 'tr._branch, tr._group',
-    hoverClass: 'hover',
-    drop: function(event, ui) {
-      $.ajax({
-        type: 'post',
-        data: {
-          clone: event.originalEvent.shiftKey,
-          source: $(ui.draggable).attr('id'),
-          target: $(this).attr('id'),
-          authenticity_token: window._token
-        },
-        url: '/admin/branches/move',
-        success: function(html) {
-          $('#branches').html(html);
-        },
-        error: function() {
-          alert('error')
-        }
-      });
-    }
-  });
   
   $('#branches tr._group').draggable({
     revert: 'invalid',
-    handle: '._move',
+    handle: '._move ._clone',
     helper: function() {
       return "<div class='helper'>"+$(this).attr('rel')+"</div>"
     },
-    drag: function(event, ui) {
-      if (event.originalEvent.shiftKey) {
+    start: function(event, ui) {
+      if ($(event.originalEvent.originalTarget).hasClass('_clone')) {
         $(ui.helper).addClass('gclone');
-      } else {
-        $(ui.helper).removeClass('gclone');
       }
     }
   });
@@ -56,4 +53,18 @@ function branches() {
       return "<div class='helper'>"+$(this).attr('rel')+"</div>"
     },    
   });
+  
+  $('#branches ._close').live('click', function() {
+    var cls = $(this).attr('rel');
+    $('.'+cls).hide();
+    $(this).hide();
+    $(this).parent().find('._open').show();
+  });
+  
+  $('#branches ._open').live('click', function() {
+    var cls = $(this).attr('rel');
+    $('.'+cls).show();
+    $(this).hide();
+    $(this).parent().find('._close').show();
+  })  
 }
