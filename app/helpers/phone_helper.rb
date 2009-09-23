fc16d7fd392550bff04c26b920856ba40a63dc99
Danil Pismenny <danil@orionet.ru>
2009-09-23 22:05:42 +0400

    вместо _search_results использую _list, некоторые изменения в выводе списка компаний

--- app/helpers/phone_helper.rb
+++ app/helpers/phone_helper.rb
@@ -2,13 +2,22 @@
 module PhoneHelper
 #  extend ActiveSupport::Memoizable
   
+  def h_phones(phones)
+    phones.map{ |p| h_phone(p) }.join(', ')
+  end
+  
   def h_phone(phone)
     return '' unless phone    
     str = phone.to_s
     if str=~/^79/
       "7 (#{str[1..3]}) #{str[4..6]}-#{str[7..8]}-#{str[9..10]}"
     else
+      # TOFIX сделать нормально
+      if str[1..4]=="8352"
+        "#{str[5..6]}-#{str[7..8]}-#{str[9..10]}"
+      else
       "7 (#{str[1..4]}) #{str[5..6]}-#{str[7..8]}-#{str[9..10]}"
+      end
     end
   end    
 
